62ae09e2d6a3a0c0567bdcd9efb2fb25f4b5af12
Pete Misik <mirtec001@hotmail.com>
2021-07-27 07:57:52 -0400

    Rework screen dimensions. Remove default image. Add player rectangle. Add keyboard movement.

--- core/src/com/mirtec/platformer/MyGdxGame.java
+++ core/src/com/mirtec/platformer/MyGdxGame.java
@@ -1,31 +1,76 @@
 package com.mirtec.platformer;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame implements ApplicationListener {
 	SpriteBatch batch;
-	Texture img;
-	
+	private ShapeRenderer shapeRenderer;
+//	Texture img;
+	int playerX;
+	int playerY;
+	int playerSpeed;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
+		// Let's create a player rectangle and draw it to the screen.
+		// How about green?
+		playerX = 10;
+		playerY = 20;
+		playerSpeed = 10;
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear(0, 0, 0, 1);
+//		batch.begin();
+//		batch.end();
+
+		// Movement
+
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			playerX -= playerSpeed;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			playerX += playerSpeed;
+		}
+
+		// Drawing
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.GREEN);
+		shapeRenderer.rect(playerX, playerY, 32, 32);
+		shapeRenderer.end();
 	}
-	
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+//		img.dispose();
+		shapeRenderer.dispose();
 	}
 }
--- desktop/src/com/mirtec/platformer/desktop/DesktopLauncher.java
+++ desktop/src/com/mirtec/platformer/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mirtec.platformer.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = (config.width / 16) * 9;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
